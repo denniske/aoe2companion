178f78be5db30754320afbfe8e27bb0471772a7e
Dennis Keil <dennis.keil10@gmail.com>
2020-06-15 11:19:47 +0200

    Image Server

--- src/ci/capture.ts
+++ src/ci/capture.ts
@@ -17,11 +17,29 @@ export async function captureImage(navigation:  StackNavigationProp<RootStackPar
 
     console.log("captureScreen");
     await sleep(500);
-    const uri = await captureScreen({
-        format: "jpg",
-        quality: 0.8
-    });
-    console.log("Image saved to", uri);
+    try {
+        const uri = await captureScreen({
+            format: "jpg",
+            quality: 0.8,
+            result: "data-uri",
+            //result: "zip-base64",
+            // result: "tmpfile",
+        });
+        console.log("Image saved to", uri);
+        await fetch('http://10.0.2.2:3000/', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: 'screen-1.jpg',
+                data: uri,
+            })
+        });
+    }catch (e) {
+        console.log("Image not saved", e);
+    }
 }
 
 export async function capture(navigation:  StackNavigationProp<RootStackParamList, "Main">) {
