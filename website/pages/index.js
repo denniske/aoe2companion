7f58a80ee1b2c0df3826947caae5de4daaf84127
Dennis Keil <dennis.keil10@gmail.com>
2020-06-17 20:11:15 +0200

    Website

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,204 +1,37 @@
-import Head from 'next/head'
 import AspectRatio from 'react-aspect-ratio';
+import Layout from "../components/layout";
 
 export default function Home() {
     return (
-        <div className="container2">
-            <Head>
-                <title>AoE II Companion</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+        <Layout>
+            <div className="showcase">
+                <div>
 
-            <main>
-                <div className="container">
-                    <div className="showcase">
-                        <div>
+                    {/*<h1>The best way to connect with your friends is with <span className="main-title">AoE II Companion</span></h1>*/}
 
-                            {/*<h1>The best way to connect with your friends is with <span className="main-title">AoE II Companion</span></h1>*/}
+                    <h1><span className="main-title">AoE II Companion</span></h1>
+                    <h3>Track your games now.</h3>
+                    <br/>
 
-                            <h1><span className="main-title">AoE II Companion</span></h1>
-                            <h3>Track your games now.</h3>
-                            <br/>
-
-                            <div className="flex-container flex-rowle">
-                                <a href="https://play.google.com/store/apps/details?id=app.fiftynineseconds.game">
-                                    <img src="/app-button-play-store.png" className="app-button app-button-play-store" />
-                                </a>
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <a href="https://apps.apple.com/app/id1489505410">
-                                    <img src="/app-button-app-store.png" className="app-button app-button-app-store" />
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className="spacer"/>
-
-                        <div className="phone-container">
-                            <AspectRatio ratio="360/718" style={{maxWidth: '400px'}}>
-                                <div className="frame"/>
-                            </AspectRatio>
-                        </div>
+                    <div className="flex-container flex-rowle">
+                        <a href="https://play.google.com/store/apps/details?id=app.fiftynineseconds.game">
+                            <img src="/app-button-play-store.png" className="app-button app-button-play-store" />
+                        </a>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href="https://apps.apple.com/app/id1489505410">
+                            <img src="/app-button-app-store.png" className="app-button app-button-app-store" />
+                        </a>
                     </div>
                 </div>
-            </main>
 
-            <footer>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <img src="/vercel.svg" alt="Vercel Logo" className="logo"/>
-                </a>
-            </footer>
+                <div className="spacer"/>
 
-            <style jsx>{`
-      
-        .container2 {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-            <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-        </div>
+                <div className="phone-container">
+                    <AspectRatio ratio="360/718" style={{maxWidth: '400px'}}>
+                        <div className="frame"/>
+                    </AspectRatio>
+                </div>
+            </div>
+        </Layout>
     )
 }
