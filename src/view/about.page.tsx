6c7c3fe40a3c40b21c86eb061f4b681d4d2c3443
Dennis Keil <dennis.keil10@gmail.com>
2020-06-23 13:55:40 +0200

    Enable in-app update

--- src/view/about.page.tsx
+++ src/view/about.page.tsx
@@ -1,51 +1,113 @@
-import React from 'react';
-import { Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+    Linking, Modal, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View
+} from 'react-native';
 import Constants from 'expo-constants';
-import { useLinkTo } from '@react-navigation/native';
+import {useLinkTo} from '@react-navigation/native';
+import {checkForUpdateAsync, fetchUpdateAsync, reloadAsync} from "expo-updates";
+import {Button} from "react-native-paper";
+import {Manifest} from "expo-updates/build/Updates.types";
 
 export default function AboutPage() {
     const linkTo = useLinkTo();
+    const [updateManifest, setUpdateManifest] = useState<Manifest>();
+    const [updating, setUpdating] = useState(false);
+    const [updateModalVisible, setUpdateModalVisible] = useState(true);
+
+    const init = async () => {
+        const update = await checkForUpdateAsync();
+        if (update.isAvailable) {
+            setUpdateManifest(update.manifest);
+        }
+    };
+
+    useEffect(() => {
+        if (!__DEV__) {
+            init();
+        }
+    });
+
+    const fetchUpdate = async () => {
+        await fetchUpdateAsync();
+        setUpdateModalVisible(true);
+        setUpdating(false);
+    };
+
+    const update = async () => {
+        await reloadAsync();
+    };
+
+    const closeUpdateModal = () => {
+        setUpdateModalVisible(false);
+    };
 
     return (
-            <View style={styles.container}>
-                <Text style={styles.title}>AoE II Companion</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>AoE II Companion</Text>
 
-                <Text style={styles.heading}>Created by</Text>
-                <Text style={styles.content}>Dennis Keil</Text>
-                <Text style={styles.content}>Niklas Ohlrogge</Text>
+            <Text style={styles.heading}>Created by</Text>
+            <Text style={styles.content}>Dennis Keil</Text>
+            <Text style={styles.content}>Niklas Ohlrogge</Text>
 
-                <Text style={styles.heading}>Version</Text>
-                <Text style={styles.content}>{Constants.manifest.releaseChannel || 'dev'}-{Constants.manifest.version}n{Constants.nativeAppVersion}+{Constants.nativeBuildVersion}</Text>
+            <Text style={styles.heading}>Version</Text>
+            <Text
+                style={styles.content}>{Constants.manifest.releaseChannel || 'dev'}-{Constants.manifest.version}n{Constants.nativeAppVersion}+{Constants.nativeBuildVersion}</Text>
 
-                <Text/>
-                <View style={styles.row}>
-                    <Text style={styles.content}>Data from </Text>
-                    <TouchableOpacity onPress={() => Linking.openURL('https://aoe2.net')}>
-                        <Text style={styles.link}>aoe2.net</Text>
-                    </TouchableOpacity>
-                </View>
+            {
+                updateManifest &&
+                <Button onPress={fetchUpdate}>Update to {updateManifest.version}</Button>
+            }
+            {
+                updating &&
+                <Text style={styles.content}>Loading Update...</Text>
+            }
+            <Modal animationType="none" transparent={true} visible={updateModalVisible}>
+                <TouchableWithoutFeedback onPress={closeUpdateModal}>
+                    <View style={styles.centeredView}>
+                        <TouchableWithoutFeedback onPress={() => {}}>
+                            <View style={styles.modalView}>
+                                <Text style={styles.content}>Do you want to restart now to apply the update?</Text>
+                                <Text/>
+                                <View style={styles.buttonRow}>
+                                    <Button style={styles.button} onPress={closeUpdateModal} mode="outlined">Restart Later</Button>
+                                    <Button style={styles.button} onPress={update} mode="contained">Restart</Button>
+                                </View>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
 
-                <Text/>
-                <View style={styles.row}>
-                    <Text style={styles.content}>Icons from </Text>
-                    <TouchableOpacity onPress={() => Linking.openURL('https://github.com/madebybowtie/FlagKit')}>
-                        <Text style={styles.link}>FlagKit</Text>
-                    </TouchableOpacity>
-                </View>
-
-                <Text/>
-                <View style={styles.row}>
-                    <TouchableOpacity onPress={() => linkTo('/privacy')}>
-                        <Text style={styles.link}>Privacy Policy</Text>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={styles.expanded}/>
-
-                <Text style={styles.textJustify}>This app is not affiliated with or endorsed by Microsoft Corporation. Age of Empires II: HD and Age of Empires II: Definitive Edition are trademarks or
-                    registered trademarks of Microsoft Corporation in the U.S. and other countries.</Text>
-
+            <Text/>
+            <View style={styles.row}>
+                <Text style={styles.content}>Data from </Text>
+                <TouchableOpacity onPress={() => Linking.openURL('https://aoe2.net')}>
+                    <Text style={styles.link}>aoe2.net</Text>
+                </TouchableOpacity>
             </View>
+
+            <Text/>
+            <View style={styles.row}>
+                <Text style={styles.content}>Icons from </Text>
+                <TouchableOpacity onPress={() => Linking.openURL('https://github.com/madebybowtie/FlagKit')}>
+                    <Text style={styles.link}>FlagKit</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Text/>
+            <View style={styles.row}>
+                <TouchableOpacity onPress={() => linkTo('/privacy')}>
+                    <Text style={styles.link}>Privacy Policy</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.expanded}/>
+
+            <Text style={styles.textJustify}>This app is not affiliated with or endorsed by Microsoft Corporation. Age
+                of Empires II: HD and Age of Empires II: Definitive Edition are trademarks or
+                registered trademarks of Microsoft Corporation in the U.S. and other countries.</Text>
+
+        </View>
     );
 }
 
@@ -73,6 +135,15 @@ const styles = StyleSheet.create({
     row: {
         flexDirection: 'row',
     },
+    buttonRow: {
+        flexDirection: 'row',
+        alignSelf: 'stretch',
+        // backgroundColor: 'red',
+        justifyContent: "flex-end",
+    },
+    button: {
+        marginLeft: 10,
+    },
     link: {
         color: '#397AF9',
     },
@@ -81,5 +152,26 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         alignItems: 'center',
         padding: 20,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    modalView: {
+        margin: 0,
+        backgroundColor: "white",
+        borderRadius: 5,
+        padding: 15,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
     },
 });
