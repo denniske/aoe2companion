0d7575bb935b95a3138a29f56a30a607f215bd9f
Dennis Keil <dennis.keil10@gmail.com>
2020-07-06 22:00:14 +0200

    Center text in TabBarLabel

--- src/view/components/tab-bar-label.tsx
+++ src/view/components/tab-bar-label.tsx
@@ -1,10 +1,18 @@
 import {useNavigation} from "@react-navigation/native";
-import {Text} from "react-native";
+import {StyleSheet, Text} from "react-native";
 import React from "react";
+
 
 export function TabBarLabel({ title, focused, color }: any) {
     const navigation = useNavigation();
     return (
-        <Text style={{fontSize: 13, color: color}} onPress={() => navigation.navigate(title)} >{title.toUpperCase()}</Text>
+        <Text style={[styles.label, { color: color }]} onPress={() => navigation.navigate(title)}>{title.toUpperCase()}</Text>
     );
 }
+
+const styles = StyleSheet.create({
+    label: {
+        fontSize: 13,
+        textAlign: 'center',
+    },
+});
