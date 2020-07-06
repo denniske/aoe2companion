efe8ba55dd2b66dd08aa7dd8f4e7c7c289cdb4bd
Dennis Keil <dennis.keil10@gmail.com>
2020-07-07 00:16:18 +0200

    Extract tech tree component

--- src/view/civ.page.tsx
+++ src/view/civ.page.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Image, ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {Civ, civs, getCivHasTech, getCivHasUnit, getCivHistoryImage, getCivIconByIndex} from "../helper/civs";
+import {Civ, civs, getCivHistoryImage, getCivIconByIndex} from "../helper/civs";
 import {RouteProp, useNavigation, useRoute} from "@react-navigation/native";
 import {RootStackParamList, RootStackProp} from "../../App";
-import {getUnitIcon, getUnitLineForUnit, getUnitLineNameForUnit, getUnitName, Unit, units} from "../helper/units";
+import {getUnitLineNameForUnit, getUnitName, Unit, units} from "../helper/units";
 import {aoeCivKey, aoeData, aoeStringKey} from "../data/data";
-import {getTechIcon, getTechName, Tech, techList} from "../helper/techs";
+import {getTechName, Tech, techList} from "../helper/techs";
 import {escapeRegExpFn} from "../helper/util";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import IconHeader from "./navigation-header/icon-header";
 import TextHeader from "./navigation-header/text-header";
+import {TechTree} from "./components/tech-tree";
 
 
 export function CivTitle(props: any) {
@@ -76,196 +76,8 @@ export function CivDetails({civ}: {civ: aoeCivKey}) {
             <Text style={styles.content}>{civType}</Text>
             <Text/>
             <Text style={styles.content}>{texts}</Text>
-
-            <Text style={styles.sectionHeader}>Tech Tree</Text>
-
-            <Text style={styles.heading}>Blacksmith</Text>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} tech="Forging"/>
-                <Ability2 civ={civ} tech="ScaleMailArmor"/>
-                <Ability2 civ={civ} tech="ScaleBardingArmor"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} tech="Fletching"/>
-                <Ability2 civ={civ} tech="PaddedArcherArmor"/>
-            </View>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} tech="IronCasting"/>
-                <Ability2 civ={civ} tech="ChainMailArmor"/>
-                <Ability2 civ={civ} tech="ChainBardingArmor"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} tech="BodkinArrow"/>
-                <Ability2 civ={civ} tech="LeatherArcherArmor"/>
-            </View>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} tech="BlastFurnace"/>
-                <Ability2 civ={civ} tech="PlateMailArmor"/>
-                <Ability2 civ={civ} tech="PlateBardingArmor"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} tech="Bracer"/>
-                <Ability2 civ={civ} tech="RingArcherArmor"/>
-            </View>
-
-            <Text style={styles.heading}>Other</Text>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} tech="Bloodlines"/>
-                <Ability2 civ={civ} tech="Husbandry"/>
-                <Ability0 civ={civ}/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} tech="ThumbRing"/>
-            </View>
-
-            <Text style={styles.heading}>Siege</Text>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="BatteringRam"/>
-                <Ability2 civ={civ} unit="Mangonel"/>
-                <Ability2 civ={civ} unit="Scorpion"/>
-                <Ability0 civ={civ}/>
-            </View>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="CappedRam"/>
-                <Ability2 civ={civ} unit="Onager"/>
-                <Ability2 civ={civ} unit="HeavyScorpion"/>
-                <Ability0 civ={civ}/>
-            </View>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="SiegeRam"/>
-                <Ability2 civ={civ} unit="SiegeOnager"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} unit="BombardCannon"/>
-                <Ability2 civ={civ} tech="SiegeEngineers"/>
-            </View>
-
-
-            <Text style={styles.heading}>Infantry & Cavalry</Text>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="LongSwordsman"/>
-                <Ability2 civ={civ} unit="Spearman"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} unit="ScoutCavalry"/>
-                <Ability2 civ={civ} unit="Knight"/>
-                <Ability2 civ={civ} unit="CamelRider"/>
-            </View>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="TwoHandedSwordsman"/>
-                <Ability2 civ={civ} unit="Pikeman"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} unit="LightCavalry"/>
-                <Ability2 civ={civ} unit="Cavalier"/>
-                <Ability2 civ={civ} unit="HeavyCamelRider"/>
-            </View>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="Champion"/>
-                <Ability2 civ={civ} unit="Halberdier"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} unit="Hussar"/>
-                <Ability2 civ={civ} unit="Paladin"/>
-                <Ability2 civ={civ} unit="ImperialCamelRider"/>
-            </View>
-
-            <Text style={styles.heading}>Archer</Text>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="Archer"/>
-                <Ability2 civ={civ} unit="Skirmisher"/>
-                <Ability0 civ={civ}/>
-                <Ability0 civ={civ}/>
-            </View>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="Crossbowman"/>
-                <Ability2 civ={civ} unit="EliteSkirmisher"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} unit="CavalryArcher"/>
-            </View>
-            <View style={styles.row2}>
-                <Ability2 civ={civ} unit="Arbalester"/>
-                <Ability2 civ={civ} unit="ImperialSkirmisher"/>
-                <Ability0 civ={civ}/>
-                <Ability2 civ={civ} unit="HeavyCavalryArcher"/>
-                <Ability2 civ={civ} unit="HandCannoneer"/>
-            </View>
+            <TechTree civ={civ} />
         </View>
-    );
-}
-
-interface AbilityProps {
-    civ: Civ;
-    tech?: Tech;
-    unit?: Unit;
-}
-interface AbilityHelperProps {
-    tech?: Tech;
-    unit?: Unit;
-}
-
-function getAbilityEnabled({civ, tech, unit}: AbilityProps) {
-    if (tech) {
-        return getCivHasTech(civ, tech);
-    }
-    if (unit) {
-        return getCivHasUnit(civ, unit);
-    }
-    return false;
-}
-
-function getAbilityIcon({tech, unit}: AbilityHelperProps) {
-    if (tech) {
-        return getTechIcon(tech);
-    }
-    if (unit) {
-        return getUnitIcon(unit);
-    }
-    return false;
-}
-
-function getAbilityNavCallback({tech, unit}: AbilityHelperProps) {
-    const navigation = useNavigation<RootStackProp>();
-    if (tech) {
-        return () => navigation.push('Tech', {tech: tech});
-    }
-    if (unit) {
-        return () => navigation.push('Unit', {unit: getUnitLineForUnit(unit)!.units[0]});
-    }
-    return () => {};
-}
-
-function Ability0({civ, tech, unit}: AbilityProps) {
-    return (
-        <View style={[styles.imageContainer0, {borderColor: 'transparent'}]}/>
-    );
-}
-
-function Ability({civ, tech, unit}: AbilityProps) {
-    const enabled = getAbilityEnabled({civ, tech, unit});
-    return (
-        <View style={styles.imageContainer2}>
-        <ImageBackground source={getAbilityIcon({tech, unit})} imageStyle={styles.imageInner2} style={styles.image2}>
-            {
-                enabled &&
-                <Text style={styles.imageText}>+4</Text>
-            }
-        </ImageBackground>
-        </View>
-    );
-}
-
-function Ability2({civ, tech, unit}: AbilityProps) {
-    const enabled = getAbilityEnabled({civ, tech, unit});
-    const color = enabled ? '#555' : '#C00';
-    const opacity = enabled ? 1 : 0.4;
-    return (
-        <TouchableOpacity style={[styles.imageContainer2, {borderColor: color, opacity: opacity}]} onPress={getAbilityNavCallback({tech, unit})}>
-            <ImageBackground source={getAbilityIcon({tech, unit})} imageStyle={styles.imageInner2} style={styles.image2}>
-                {
-                    !enabled &&
-                    <Icon name={'close'} size={40} color="red" />
-                }
-            </ImageBackground>
-        </TouchableOpacity>
-    );
-}
-
-function AbilityText({civ, tech, unit}: AbilityProps) {
-    return (
-        <Text style={styles.content}>Has Bloodlines: {getCivHasTech(civ, tech!) ? 'true' : 'false'}</Text>
     );
 }
 
@@ -274,14 +86,13 @@ export function CivList() {
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <View style={styles.civContainer}>
+            <View style={styles.civList}>
                 {
                     civs.map((civ, i) =>
                         <TouchableOpacity key={civ} onPress={() => navigation.push('Civ', {civ})}>
                             <View style={styles.civBlock}>
                                 <Image style={styles.icon} source={getCivIconByIndex(i)}/>
                                 <Text style={styles.name}>{civ}</Text>
-                                {/*<Text style={styles.name}>{civ} {getCivHasTech(civ, 'Supplies') ? '' : 'FALSE'}</Text>*/}
                             </View>
                         </TouchableOpacity>
                     )
@@ -314,65 +125,14 @@ const styles = StyleSheet.create({
         marginBottom: 15,
         fontSize: 15,
         fontWeight: '500',
-        // textAlign: 'center',
-    },
-    imageInner2: {
-        // opacity: 0.1,
-        resizeMode: "cover",
-        alignSelf: 'flex-end',
-        // bottom: -50,
-        // right: 0,
-        // left: undefined,
-        // top: undefined,
-        // height: 400,
-        // width: '100%',
-    },
-    imageText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#F00',
-        // fontFamily: 'Arial',
-        textShadowColor: 'black',
-        textShadowOffset: {width: 2, height: 2},
-        textShadowRadius: 3,
-        // textAlignVertical: 'center',
-        // height: '100%',
-        // backgroundColor: 'red'
-    },
-    image2: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 38,
-        height: 38,
-        resizeMode: "contain",
-        // backgroundColor: 'blue',
-    },
-    imageContainer0: {
-        borderWidth: 3,
-        borderColor: '#FFF',
-        width: 44,
-        height: 44,
-        margin: 2,
-        // backgroundColor: 'blue',
-    },
-    imageContainer2: {
-        borderWidth: 3,
-        borderColor: '#555',
-        width: 44,
-        height: 44,
-        margin: 2,
-        // backgroundColor: 'blue',
     },
     imageInner: {
         opacity: 0.1,
         resizeMode: "cover",
         alignSelf: 'flex-end',
         bottom: -50,
-        // right: 0,
-        // left: undefined,
         top: undefined,
         height: 400,
-        // width: '100%',
     },
     image: {
         flex: 1,
@@ -399,10 +159,9 @@ const styles = StyleSheet.create({
     },
     detailsContainer: {
         flex: 1,
-        // backgroundColor: 'yellow',
         padding: 20,
+        // backgroundColor: 'yellow',
     },
-
     icon: {
       width: 30,
       height: 30,
@@ -413,32 +172,16 @@ const styles = StyleSheet.create({
     },
     civBlock: {
         flexDirection: 'row',
-        // justifyContent: 'center',
         alignItems: 'center',
-        // flex: 1,
         marginHorizontal: 0,
         marginVertical: 5,
     },
-    civContainer: {
-        // flexDirection: 'row',
-        // flexWrap: 'wrap',
+    civList: {
     },
     container: {
-        // flex: 1,
         backgroundColor: 'white',
         alignItems: 'center',
         padding: 20,
-    },
-
-    rowSpacer: {
-        marginBottom: 10,
-    },
-    row2: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 2,
-        marginHorizontal: -2,
-        // backgroundColor: 'blue',
     },
     row: {
         marginLeft: 5,
@@ -446,9 +189,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginBottom: 5,
         // backgroundColor: 'blue',
-    },
-    unitIcon: {
-        width: 20,
-        height: 20,
     },
 });
