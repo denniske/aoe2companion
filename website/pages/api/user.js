131c22e8f44c719cb3c3ca51cffeb033b7816664
Dennis Keil <dennis.keil10@gmail.com>
2020-07-16 09:06:33 +0200

    Server

--- website/pages/api/user.js
+++ website/pages/api/user.js
@@ -1,5 +1,9 @@
+
+let counter = 0;
+
 export default (req, res) => {
+    counter++;
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ name: 'John Doe' }))
+    res.end(JSON.stringify({ name: 'John Doe', counter }))
 }
