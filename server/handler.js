1f35a68966d0c1db2206eaba86e90a7cbbd9117f
Dennis Keil <dennis.keil10@gmail.com>
2020-07-16 23:28:23 +0200

    Server sls 2

--- server/handler.js
+++ server/handler.js
@@ -7,7 +7,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Go Serverless v2.0! Your function executed successfully!',
         input: event,
       },
       null,
