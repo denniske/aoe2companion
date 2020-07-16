98dbdbc66da4862f57c9fe53051f4b8cfe7d4ef6
Dennis Keil <dennis.keil10@gmail.com>
2020-07-16 23:22:17 +0200

    Server sls

---
+++ server/handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports.hello = async event => {
+  console.log(123);
+  throw new Error('lol');
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
