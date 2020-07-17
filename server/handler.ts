e0408878ea19ba51d1d8c21820b6a082d73b8eba
Dennis Keil <dennis.keil10@gmail.com>
2020-07-17 11:41:44 +0200

    Server sls 2

--- server/handler.ts
+++ server/handler.ts
@@ -1,7 +1,44 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import {createConnection, getConnectionManager} from "typeorm";
+
+export async function createDB() {
+  try {
+    const connection  = await  createConnection({
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      // entities: [
+      //   User,
+      // ],
+      // entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
+      synchronize: true,
+      logging: false
+    });
+    return connection;
+  } catch (err) {
+    // If AlreadyHasActiveConnectionError occurs, return already existing connection
+    if (err.name === "AlreadyHasActiveConnectionError") {
+      const existingConnection = getConnectionManager().get("default");
+      console.log('Using existing connection. Connected: ', existingConnection.isConnected);
+      console.log('Using existing connection. Connected: ', existingConnection.entityMetadatas);
+
+      return existingConnection;
+    } else {
+      throw err;
+    }
+  }
+}
 
 export const hello3: APIGatewayProxyHandler = async (event, _context) => {
+
+  console.log('starting');
+
+  const connection = await createDB();
+  console.log(connection);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
