92e96d19a73446db47eb324a224cefbf1ebf3828
Dennis Keil <dennis.keil10@gmail.com>
2020-07-17 11:04:42 +0200

    Server sls 2

--- server/handler.ts
+++ server/handler.ts
@@ -5,7 +5,7 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message: 'Go Serverless Webpack (Typescript) v10.0! Your function executed successfully!',
       input: event,
     }, null, 2),
   };
