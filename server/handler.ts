0fe496bb7e928d35c5a435e44358a4e7eda8eff7
Dennis Keil <dennis.keil10@gmail.com>
2020-07-17 11:15:18 +0200

    Server sls 2

--- server/handler.ts
+++ server/handler.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const hello3: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
