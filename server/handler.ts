a99397f8964b73effbebad3bde984288d2664cf0
Dennis Keil <dennis.keil10@gmail.com>
2020-07-17 16:59:32 +0200

    Server sls 2

--- server/handler.ts
+++ server/handler.ts
@@ -1,8 +1,11 @@
+import "reflect-metadata";
+
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import {createConnection, getConnectionManager} from "typeorm";
 
 import * as pg2 from 'pg';
+import {User} from "./entity/user";
 
 console.log(pg2);
 
@@ -14,9 +17,9 @@ export async function createDB() {
       ssl: {
         rejectUnauthorized: false,
       },
-      // entities: [
-      //   User,
-      // ],
+      entities: [
+        User,
+      ],
       // entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
       synchronize: true,
       logging: false
@@ -43,11 +46,14 @@ export const hello3: APIGatewayProxyHandler = async (event, _context) => {
   const connection = await createDB();
   console.log(connection);
 
+  const users = await connection.manager.find(User, {where: { country: 'DE' }, skip: 0, take: 5 });
+
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Hi:' + process.env.TWITTER_ACCESS_TOKEN + '. Ho:' + process.env.TWITTER_ACCESS_TOKEN2 + '. Go Serverless Webpack (Typescript) v10.0! Your function executed successfully!',
       input: event,
+      users: users.map(u => u.data),
     }, null, 2),
   };
 }
