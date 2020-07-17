dd61864d7092ae84ee2264cd229bf0afefd9f0dd
Dennis Keil <dennis.keil10@gmail.com>
2020-07-17 11:32:34 +0200

    Server sls 2

--- server/handler.ts
+++ server/handler.ts
@@ -5,7 +5,7 @@ export const hello3: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v10.0! Your function executed successfully!',
+      message: 'Hi:' + process.env.TWITTER_ACCESS_TOKEN + '. Ho:' + process.env.TWITTER_ACCESS_TOKEN2 + '. Go Serverless Webpack (Typescript) v10.0! Your function executed successfully!',
       input: event,
     }, null, 2),
   };
