a033c425c6fe6b2c1ea17841643b483b5072fb96
Dennis Keil <dennis.keil10@gmail.com>
2020-08-04 10:59:41 +0200

    Add Ultima Gaina as supporter

--- src/view/about.page.tsx
+++ src/view/about.page.tsx
@@ -58,6 +58,7 @@ export default function AboutPage() {
             <MyText style={styles.content}>Kloakan</MyText>
             <MyText style={styles.content}>Sihing Mo</MyText>
             <MyText style={styles.content}>Sebastian Janus</MyText>
+            <MyText style={styles.content}>Ultima Gaina</MyText>
             <MyText style={styles.content2}>+ anonymous supporters</MyText>
 
             <MyText style={styles.heading}>Version</MyText>
