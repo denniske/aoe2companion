0e972815dcefdb8a96810abd5b1b8ca6892726dd
Dennis Keil <dennis.keil10@gmail.com>
2020-09-19 13:58:36 +0200

    NestJs

--- graph/deploy/_deploy.sh
+++ graph/deploy/_deploy.sh
@@ -9,6 +9,23 @@ COMMIT_SHA1=$TRAVIS_COMMIT
 # We must export it so it's available for envsubst
 export COMMIT_SHA1=$COMMIT_SHA1
 
+#############################
+# BUILD
+#############################
+
+nx build graph
+docker build -f graph/deploy/Dockerfile -t denniske/aoe2companion-$SERVICE_NAME:$TRAVIS_COMMIT .
+echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+docker push denniske/aoe2companion-$SERVICE_NAME:$TRAVIS_COMMIT
+
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+
+#############################
+# DEPLOY
+#############################
+
+cd graph/deploy
+
 # Since the only way for envsubst to work on files is using input/output redirection,
 # it's not possible to do in-place substitution, so we need to save the output to another file
 # and overwrite the original with that one.
@@ -18,13 +35,6 @@ envsubst <./ingress.yml >./ingress.yml.out
 mv ./ingress.yml.out ./ingress.yml
 envsubst <./service.yml >./service.yml.out
 mv ./service.yml.out ./service.yml
-
-nx build graph
-docker build -f graph/deploy/Dockerfile -t denniske/aoe2companion-$SERVICE_NAME:$TRAVIS_COMMIT .
-echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-docker push denniske/aoe2companion-$SERVICE_NAME:$TRAVIS_COMMIT
-
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
 kubectl \
   --kubeconfig=/dev/null \
