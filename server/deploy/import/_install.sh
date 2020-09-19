22654148675056633918d80f0923fb81bd899dfa
Dennis Keil <dennis.keil10@gmail.com>
2020-09-19 20:15:36 +0200

    REMOVE SERVER AND SERVERLESS

--- server/deploy/import/_install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
-sudo apt-get update
-sudo apt-get -y install gettext-base
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod u+x ./kubectl
-sudo mv kubectl /usr/local/bin/
