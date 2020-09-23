0bf870c1b0d106fbbeac935bd8a68cc235be19a8
Dennis Keil <dennis.keil10@gmail.com>
2020-09-23 19:30:18 +0200

    Civs

--- website/next.config.js
+++ website/next.config.js
@@ -3,4 +3,6 @@ console.log('TRANSPILE');
 
 const withTM = require('next-transpile-modules')(['../data']); // pass the modules you would like to see transpiled
 
-module.exports = withTM();
+module.exports = withTM({
+    target: 'server',
+});
