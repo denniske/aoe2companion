e7d0603f1129bdd9f76c1938c660aa76c3bf5384
Dennis Keil <dennis.keil10@gmail.com>
2020-09-27 03:10:27 +0200

    Fix babel config for app

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,19 @@
-// module.exports = function(api) {
-//   api.cache(true);
-//   return {
-//     presets: ['babel-preset-expo'],
-//     plugins: [
-//       [
-//         "module-resolver",
-//         {
-//           alias: {
-//             "@nex/data": ["./data/src/index.ts"],
-//           }
-//         }
-//       ]
-//     ]
-//   };
-// };
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@nex/data/hooks": ["./data/src/index-hooks.ts"],
+            "@nex/data": ["./data/src/index.ts"],
+          }
+        }
+      ]
+    ]
+  };
+};
 
-module.exports = {};
+// module.exports = {};
