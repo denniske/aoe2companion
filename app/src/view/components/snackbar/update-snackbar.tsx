b79d3d0d7cde43a4a0af37a62e4189be0d8afc31
Dennis Keil <dennis.keil10@gmail.com>
2021-02-08 12:44:33 +0100

    Fix update store version string

--- app/src/view/components/snackbar/update-snackbar.tsx
+++ app/src/view/components/snackbar/update-snackbar.tsx
@@ -82,7 +82,7 @@ export default function UpdateSnackbar() {
             break;
         case 'storeUpdateAvailable':
             const store = getTranslation(`updatesnackbar.store.${Platform.OS}`);
-            message = getTranslation('updatesnackbar.update.updateavailable', { version: `v${updateManifest?.version}`, store });
+            message = getTranslation('updatesnackbar.update.updateavailable', { version: `v${updateStoreManifest.version}`, store });
             actions = [
                 {
                     label: getTranslation('updatesnackbar.action.open'),
