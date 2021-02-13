f80766e6e9c37e12a026e97f7c87167aa14031a9
Dennis Keil <dennis.keil10@gmail.com>
2021-02-13 03:22:30 +0100

    Pusher Web

--- app/src/helper/pusher.web.ts
+++ app/src/helper/pusher.web.ts
@@ -1,13 +1,22 @@
 import * as PusherPushNotifications from "@pusher/push-notifications-web"
 
+
+let myserviceWorkerRegistration: any = null;
+
+window.navigator.serviceWorker.ready.then(serviceWorkerRegistration =>
+    myserviceWorkerRegistration = serviceWorkerRegistration
+);
+
 export function initPusher() {
+    console.log('initPusher', myserviceWorkerRegistration);
+
     const beamsClient = new PusherPushNotifications.Client({
         instanceId: 'f5f0895e-446c-4fb7-9c88-cee14814718d',
+        serviceWorkerRegistration: myserviceWorkerRegistration,
     })
 
     beamsClient.start()
         .then(() => beamsClient.addDeviceInterest('hello'))
         .then(() => console.log('Successfully registered and subscribed!'))
         .catch(console.error);
-
 }
