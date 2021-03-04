e8560c58e81d44c84148f5415556885f6bee1cef
Dennis Keil <dennis.keil10@gmail.com>
2021-03-04 12:56:23 +0100

    Transpile lodash-es for website

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,7 @@
 
 // console.log('TRANSPILE');
 
-const withTM = require('next-transpile-modules')(['../data']); // pass the modules you would like to see transpiled
+const withTM = require('next-transpile-modules')(['../data', 'lodash-es']); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
     target: 'serverless',
