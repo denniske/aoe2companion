1733bebb810437a670e0c126595ad3ff3c18e341
Dennis Keil <dennis.keil10@gmail.com>
2021-03-10 11:41:14 +0100

    Comment bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(
-      {
-        ...env,
-        // Passing true will enable the default Workbox + Expo SW configuration.
-        offline: true,
-      },
-      argv
-  );
-  // Customize the config before returning it.
-    if (env.mode === 'production') {
-        config.plugins.push(
-            new BundleAnalyzerPlugin({
-                path: 'web-report',
-            })
-        );
-    }
-  return config;
+    const config = await createExpoWebpackConfigAsync(
+        {
+            ...env,
+            // Passing true will enable the default Workbox + Expo SW configuration.
+            offline: true,
+        },
+        argv
+    );
+    // Customize the config before returning it.
+    // if (env.mode === 'production') {
+    //     config.plugins.push(
+    //         new BundleAnalyzerPlugin({
+    //             path: 'web-report',
+    //         })
+    //     );
+    // }
+    return config;
 };
