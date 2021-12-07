2b2ae38f751b8fe8300e5d8810a802b3b7967e7d
Dennis Keil <dennis.keil10@gmail.com>
2021-12-07 12:35:34 +0100

    eas build

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,12 @@
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 console.log("Applying metro.config.js");
 
-module.exports = (async () => {
-    const {
-        resolver: { sourceExts, assetExts }
-    } = await getDefaultConfig();
+const defaultConfig = getDefaultConfig(__dirname);
 
-    return {
-        resolver: {
-            blacklistRE: exclusionList([/^tools\/.*/, /^website2\/.*/]),
-            assetExts: [...assetExts, 'lazy'],
-        }
-    };
-})();
+defaultConfig.resolver.blacklistRE = exclusionList([/^tools\/.*/, /^website2\/.*/]);
+defaultConfig.resolver.assetExts.push('lazy');
+
+module.exports = defaultConfig;
