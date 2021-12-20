ec199bdd45f278fae982673810066322cbe3f889
Dennis Keil <dennis.keil10@gmail.com>
2021-12-20 15:59:16 +0100

    client4

--- app.config.js
+++ app.config.js
@@ -1,4 +1,7 @@
 require('ts-node/register');
+
+console.log('APP ENV', process.env.APP);
+
 module.exports = process.env.APP === 'aoe2' ? require('./app2.config.ts') : require('./app4.config.ts');
 
 // import app2Config from "./app2.config";
