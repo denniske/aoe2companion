0fb9e05a9ecfe5e86e1716ae4758101a1fd8dafe
Dennis Keil <dennis.keil10@gmail.com>
2021-12-22 16:13:44 +0100

    client4 website

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,9 @@
 
 console.log('TRANSPILE');
 
-const withTM = require('next-transpile-modules')(['/vercel/path0/data']); // pass the modules you would like to see transpiled
+const withTM = require('next-transpile-modules')([
+    process.cwd().includes('/vercel/path0') ? '/vercel/path0/data' : '../data'
+]); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
     // target: 'serverless',
