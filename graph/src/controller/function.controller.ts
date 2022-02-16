8ca1e06fea12c764bc69b2685f7dc1ca5efc4635
Dennis Keil <dennis.keil10@gmail.com>
2022-02-16 21:19:37 +0100

    rest api

--- graph/src/controller/function.controller.ts
+++ graph/src/controller/function.controller.ts
@@ -167,4 +167,57 @@ export class FunctionController implements OnModuleInit {
         time();
         this.updateCache();
     }
+
+
+    @Get('/api/matches')
+    async matches(
+        @Request() req,
+        @Response() res,
+    ) {
+        time(1);
+        console.log('params:', req.query);
+
+        const cursor = getParam(req.query, 'cursor') ?? '0';
+        const count = parseInt(getParam(req.query, 'count') ?? '10');
+
+        console.log({
+            cursor,
+            count,
+        });
+
+        if (
+            count < 1 ||
+            count > 10000
+        ) {
+            res.send({
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'Invalid or missing params (valid count 1..10000)',
+                }, null, 2),
+            });
+            return;
+        }
+
+        time();
+
+        const matches = await this.prisma.match.findMany({
+            include: {
+                players: true,
+            },
+            where: {
+                match_id: { gt: cursor },
+            },
+            take: count,
+            orderBy: {
+                match_id: 'asc',
+            },
+        });
+        time();
+
+        res.send({
+            next_cursor: matches.length < count ? null : matches[matches.length - 1].match_id,
+            matches,
+        });
+        time();
+    }
 }
