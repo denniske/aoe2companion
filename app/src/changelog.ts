939bfc1cd319df53dd9b95b6554055742d83740e
Dennis Keil <dennis.keil10@gmail.com>
2022-08-13 11:12:20 +0200

    sdk46

--- app/src/changelog.ts
+++ app/src/changelog.ts
@@ -63,6 +63,12 @@ export const changelog4: IChangelog = {
 };
 
 export const changelog: IChangelog = {
+    '41.0.0': [
+        {
+            type: 'minor',
+            title: 'Internal Framework Update (SDK 46)',
+        },
+    ],
     '40.0.0': [
         {
             type: 'minor',
