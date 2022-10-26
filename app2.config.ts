3d9df71eef1804f1a629ca9557957810e309eca8
Dennis Keil <dennis.keil10@gmail.com>
2022-10-26 14:47:09 +0200

    update debug

--- app2.config.ts
+++ app2.config.ts
@@ -19,7 +19,7 @@ export default {
     "userInterfaceStyle": "automatic",
     "jsEngine": "hermes",
     "runtimeVersion": process.env.RUNTIME_VERSION,
-    "version": "52.0.2",
+    "version": "52.0.3",
     "orientation": "portrait",
     "privacy": "public",
     "githubUrl": "https://github.com/denniske/aoe2companion",
@@ -71,7 +71,7 @@ export default {
         "foregroundImage": "./app/assets/icon-adaptive.png"
       },
       "package": "com.aoe2companion",
-      "versionCode": 520002,
+      "versionCode": 520003,
       "permissions": [],
       "googleServicesFile": "./google-services2.json",
       "useNextNotificationsApi": true,
@@ -90,7 +90,7 @@ export default {
       "userInterfaceStyle": "automatic",
       "icon": "./app/assets/icon-adaptive-no-alpha.png",
       "bundleIdentifier": "com.aoe2companion",
-      "buildNumber": "52.0.2",
+      "buildNumber": "52.0.3",
       "supportsTablet": false,
       "config": {
         "usesNonExemptEncryption": false
