60a876e2d971e59016c9444b66b5310f207f0f4a
Dennis Keil <dennis.keil10@gmail.com>
2024-02-25 14:04:43 +0100

    update react native paper & add yarn start:private

--- app/src/helper/sentry.ts
+++ app/src/helper/sentry.ts
@@ -1,15 +1,14 @@
 import * as Sentry from '@sentry/react-native';
-import {appConfig} from "@nex/dataset";
+import { appConfig } from '@nex/dataset';
 
 export default function initSentry() {
-    if (!__DEV__) {
-        Sentry.init({
-            dsn: appConfig.sentry.dsn,
-            debug: false,
+    Sentry.init({
+        dsn: appConfig.sentry.dsn,
+        debug: false,
+        enabled: !__DEV__,
 
-            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-            // We recommend adjusting this value in production.
-            // tracesSampleRate: 1.0,
-        });
-    }
+        // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+        // We recommend adjusting this value in production.
+        // tracesSampleRate: 1.0,
+    });
 }
