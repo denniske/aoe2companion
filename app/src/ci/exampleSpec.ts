1df7f86bec02d090061927d8f16fc54e9f3387d2
Dennis Keil <dennis.keil10@gmail.com>
2024-03-15 18:13:44 +0700

    testing

--- app/src/ci/exampleSpec.ts
+++ app/src/ci/exampleSpec.ts
@@ -30,6 +30,7 @@ export default function (spec: any) {
         spec.it('all', async function () {
             await AsyncStorage.removeItem('settings');
             await AsyncStorage.setItem('following', '[{"id":"76561197984749679-196240","steam_id":"76561197984749679","profile_id":196240,"name":"GL.TheViper","games":2243,"country":"NO"},{"id":"76561198011417995-254415","steam_id":"76561198011417995","profile_id":254415,"name":"Modri","games":1168,"country":"SI"},{"id":"76561198083128303-2413974","steam_id":"76561198083128303","profile_id":2413974,"name":"Roggy","games":2298,"country":"TR"}]');
+            // await AsyncStorage.setItem('settings', '{"id":"199325","profile_id":199325}');
             // console.log(await AsyncStorage.getItem('following'));
 
             let route: string;
@@ -40,16 +41,31 @@ export default function (spec: any) {
             // await capture(routeToScreenshotName(route));
             // await sleep(1000);
 
-
             // route = `/matches/users/select?search=Hera`;
             // router.navigate(route);
             // await sleep(waitTime*2);
             // await capture(routeToScreenshotName(route));
             // await sleep(1000);
 
-            // await AsyncStorage.setItem('settings', '{"id":"199325","profile_id":199325}');
+            // route = `/matches/users/199325`;
+            // router.navigate(route);
+            // await sleep(waitTime*2);
+            // await capture(routeToScreenshotName(route));
+            // await sleep(1000);
 
-            route = `/matches/users/199325`;
+            // route = `/statistics/leaderboard`;
+            // router.navigate(route);
+            // await sleep(waitTime*2);
+            // await capture(routeToScreenshotName(route));
+            // await sleep(1000);
+
+            // route = `/explore/civilizations/Aztecs`;
+            // router.navigate(route);
+            // await sleep(waitTime*2);
+            // await capture(routeToScreenshotName(route));
+            // await sleep(1000);
+
+            route = `/explore/build-orders/17`;
             router.navigate(route);
             await sleep(waitTime*2);
             await capture(routeToScreenshotName(route));
@@ -77,26 +93,26 @@ export default function (spec: any) {
             // await sleep(waitTime);
             // await capture();
 
-            navigation.reset({index: 0, routes: [{name: 'Leaderboard'}]});
-            await sleep(1000);
-            await sleep(waitTime);
-            await capture();
+            // navigation.reset({index: 0, routes: [{name: 'Leaderboard'}]});
+            // await sleep(1000);
+            // await sleep(waitTime);
+            // await capture();
 
             // navigation.reset({index: 0, routes: [{name: 'Guide'}]});
             // await sleep(1000);
             // await sleep(waitTime);
             // await capture();
 
-            navigation.reset({index: 0, routes: [{name: 'Civ'}]})
-            await sleep(1000);
-            await sleep(waitTime);
-            await capture();
+            // navigation.reset({index: 0, routes: [{name: 'Civ'}]})
+            // await sleep(1000);
+            // await sleep(waitTime);
+            // await capture();
 
-            navigation.reset({index: 0, routes: [{name: 'Civ'}, {name: 'Civ', params: {civ: 'Chinese'}}]})
-            // navigation.reset({index: 0, routes: [{name: 'Civ'}, {name: 'Civ', params: {civ: 'Aztecs'}}]})
-            await sleep(1000);
-            await sleep(waitTime);
-            await capture();
+            // navigation.reset({index: 0, routes: [{name: 'Civ'}, {name: 'Civ', params: {civ: 'Chinese'}}]})
+            // // navigation.reset({index: 0, routes: [{name: 'Civ'}, {name: 'Civ', params: {civ: 'Aztecs'}}]})
+            // await sleep(1000);
+            // await sleep(waitTime);
+            // await capture();
 
             // navigation.reset({index: 0, routes: [{name: 'Unit'}, {name: 'Unit', params: {unit: 'Arambai'}}]})
             // await sleep(1000);
