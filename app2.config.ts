32d392af8f8a0f04fcc3a5b17a108230cde4572e
Dennis Keil <dennis.keil10@gmail.com>
2024-05-23 20:28:14 +0700

    v102

--- app2.config.ts
+++ app2.config.ts
@@ -19,7 +19,7 @@ const sentryConfigPlugin = [
     }
 ];
 
-const version = '101.0.0';
+const version = '102.0.0';
 const versionParts = version.split('.');
 
 const runtimeVersion = versionParts[0] + '.' + versionParts[1] + '.0';
