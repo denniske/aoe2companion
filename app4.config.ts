28fa4bc3ea157c648158d14f6a55085a868f9ef0
Dennis Keil <dennis.keil10@gmail.com>
2024-05-24 14:35:44 +0700

    aoe 4 v15

--- app4.config.ts
+++ app4.config.ts
@@ -19,7 +19,7 @@ const sentryConfigPlugin = [
     }
 ];
 
-const version = '14.0.0';
+const version = '15.0.0';
 const versionParts = version.split('.');
 
 const runtimeVersion = versionParts[0] + '.' + versionParts[1] + '.0';
