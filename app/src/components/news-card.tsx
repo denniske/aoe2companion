703266b13f0a6e1a3693dd092d24fc6744af2596
Dennis Keil <dennis.keil10@gmail.com>
2024-10-19 17:26:23 +0200

    Load smaller version of news images

--- app/src/components/news-card.tsx
+++ app/src/components/news-card.tsx
@@ -1,26 +1,40 @@
 import { Post, useMedia } from '@app/utils/news';
 import { Image } from 'expo-image';
 import { decode } from 'html-entities';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 
 import { Card } from './card';
 import { NewsPopup } from './news-popup';
 import { Skeleton, SkeletonText } from './skeleton';
 import { Text } from './text';
+import { sortBy } from 'lodash';
 
 export const NewsCard: React.FC<Post> = (post) => {
     const { data } = useMedia(post.featured_media);
     const [visible, setVisible] = useState(false);
+    const [imageUrl, setImageUrl] = useState<string>();
 
     if (!post.id) {
         return <NewsCardSkeleton />;
     }
 
+    useEffect(() => {
+        if (data) {
+            if (data.media_details?.sizes) {
+                const validSizes = Object.values(data.media_details.sizes).filter((size) => size.width > 500);
+                const orderedSizes = sortBy(validSizes, s => s.filesize);
+                setImageUrl(orderedSizes[0]?.source_url);
+            } else {
+                setImageUrl(data.source_url);
+            }
+        }
+    }, [data]);
+
     return (
         <Card onPress={() => setVisible(true)} direction="vertical" className="overflow-hidden w-56">
             <View className="-mx-2 -mt-3 mb-1">
-                <Image source={{ uri: data?.source_url }} className="w-full" style={{ aspectRatio: 1.75 }} />
+                <Image source={{ uri: imageUrl }} className="w-full" style={{ aspectRatio: 1.75 }} />
             </View>
             <Text variant="label" numberOfLines={2}>
                 {decode(post.title.rendered)}
