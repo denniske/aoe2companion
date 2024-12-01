d60b8595e791d682a9e54eecf2ae7613cdc37693
Dennis Keil <dennis.keil10@gmail.com>
2024-12-01 13:27:34 +0100

    account following

--- app/src/view/components/badge/discord-badge.tsx
+++ app/src/view/components/badge/discord-badge.tsx
@@ -19,8 +19,6 @@ export default function DiscordBadge(props: Props) {
         enabled: !!invitationId && !__DEV__,
     });
 
-    console.log('__DEV__', __DEV__);
-
     let content = undefined;
     if (info?.approximate_presence_count) {
         content = `${info?.approximate_presence_count} online`;
