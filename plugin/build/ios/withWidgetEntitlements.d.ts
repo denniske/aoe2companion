278d52efae49085b293d6bd6216c025d118b03d0
Dennis Keil <dennis.keil10@gmail.com>
2025-06-12 17:54:37 +0200

    remove plugin/build

--- plugin/build/ios/withWidgetEntitlements.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigPlugin } from "@expo/config-plugins";
-interface Props {
-    appGroupIdentifier: string;
-    targetName: string;
-}
-export declare const withWidgetEntitlements: ConfigPlugin<Props>;
-export {};
--- plugin/build/ios/withWidgetIos.d.ts
+++
@@ -1,3 +0,0 @@
-import { ConfigPlugin } from '@expo/config-plugins';
-import { Props } from '..';
-export declare const withWidgetIos: ConfigPlugin<Props>;
