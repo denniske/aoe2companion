7900faea12b160b8e44d3e809f0d1ea1bf3371a2
Dennis Keil <dennis.keil10@gmail.com>
2025-07-30 11:30:40 +0200

    winrate card padding

--- app/src/app/statistics/index.tsx
+++ app/src/app/statistics/index.tsx
@@ -24,13 +24,13 @@ export default function StatisticsPage() {
             />
 
             <Card href="/statistics/leaderboard" direction="vertical" className="overflow-hidden">
-                <View className="-mx-2 -mt-3 mb-1">
+                <View className="-mx-4 -mt-4 mb-1">
                     <Image source={require('../../../assets/statistics/hero.webp')} className="h-36 w-full" />
                 </View>
                 <Text variant="label-lg">{getTranslation('leaderboards.title')}</Text>
             </Card>
             <Card href={Platform.OS === 'web' ? 'https://aoestats.io' : '/statistics/winrates'} direction="vertical" className="overflow-hidden">
-                <View className="-mx-2 -mt-3 mb-1">
+                <View className="-mx-4 -mt-4 mb-1">
                     <Image source={require('../../../assets/statistics/armenians.webp')} className="h-36 w-full" />
                 </View>
                 <Text variant="label-lg">{getTranslation('winrates.title')}</Text>
