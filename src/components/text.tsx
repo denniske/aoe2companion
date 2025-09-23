86c7913866d5fcf99a1a8de443387ce6df570817
Dennis Keil <dennis.keil10@gmail.com>
2025-09-23 23:01:42 +0200

    fix text align prop

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -10,52 +10,13 @@ export interface TextProps extends RNTextProps {
 export const Text: React.FC<TextProps> = ({ variant = 'body', color = 'default', align, style, className, ...props }) => {
     const textColor = textColors[color] ?? color;
 
-    // if (props.children === 'Matches' && className?.includes('!text-[9px]')) {
-    //     console.log('text', props.children, `${className ?? ''} ${textColor} `, style);
-    //
-    // }
-
-    //  ${textColor}
-
-    // console.log('text', props.children, `${className ?? ''} ${textColor} `);
-
     const style2 = {
-        textAlign: align,
+        ...(align ? {textAlign: align} : {}),
         ...textVariantStyles[variant],
         ...(style as any ?? {}),
     }
 
-    // return <RNText className={`${textColor}`} style={[{ textAlign: 'center' }]}>Test</RNText>;
-    // return <RNText className={`${textColor}`} style={[{ textAlign: align }]} {...props} />;
-    return <RNText className={`${className} ${textColor} `} style={style2} {...props} />;
+    // console.log('text', props.children, style2);
+
+    return <RNText className={`${className} ${textColor}`} style={style2} {...props} />;
 };
-
-// export const Text: React.FC<TextProps> = ({ color, children }) => {
-//     const textColor = textColors[color as any];
-//
-//     // if (props.children === 'Matches' && className?.includes('!text-[9px]')) {
-//     //     console.log('text', props.children, `${className ?? ''} ${textColor} `, style);
-//     //
-//     // }
-//
-//     //  ${textColor}
-//
-//     return <RNText className={textColor} style={[{ textAlign: 'center' }]}>{children}</RNText>;
-//     // return <RNText className={`${textColor}`} style={[{ textAlign: align }]} {...props} />;
-//     // return <RNText className={`${className} ${textColor} `} style={[{ textAlign: align }, textVariantStyles[variant], style]} {...props} />;
-// };
-
-// export const Text: React.FC<TextProps> = ({ className, children }) => {
-//     // const textColor = textColors[color as any];
-//
-//     // if (props.children === 'Matches' && className?.includes('!text-[9px]')) {
-//     //     console.log('text', props.children, `${className ?? ''} ${textColor} `, style);
-//     //
-//     // }
-//
-//     //  ${textColor}
-//
-//     return <RNText className={className} style={{ textAlign: 'center' }}>{children}</RNText>;
-//     // return <RNText className={`${textColor}`} style={[{ textAlign: align }]} {...props} />;
-//     // return <RNText className={`${className} ${textColor} `} style={[{ textAlign: align }, textVariantStyles[variant], style]} {...props} />;
-// };
