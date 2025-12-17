c64c8252f5aaec7ea16fd61019ecc6713ed2828f
Noah Brandyberry <noahbrandyberry@gmail.com>
2025-12-17 12:43:02 -0500

    chore: implement web leaderboards

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,38 @@
-import { View } from 'react-native';
+import { Platform, Pressable, View } from 'react-native';
 import cn from 'classnames';
 import { containerClassName } from '@app/styles';
 import { Text } from './text';
-import { Href, Link as ExpoLink } from 'expo-router';
-import { appConfig } from '@nex/dataset';
+import { Href, Link as ExpoLink, router } from 'expo-router';
+import { appConfig, appIconData } from '@nex/dataset';
 import { Link } from './link';
 import { useTranslation } from '@app/helper/translate';
+import { useAuthProfileId, useFollowedAndMeProfileIds } from '@app/queries/all';
+import { Image } from './uniwind/image';
 
 export const Footer: React.FC = () => {
     const getTranslation = useTranslation();
+    const authProfileId = useAuthProfileId();
+    const profileIds = useFollowedAndMeProfileIds();
 
     return (
-        <View className="bg-[#F3EFE6] dark:bg-blue-900">
-            <View className={cn('py-4 flex-row', containerClassName)}>
+        <View className="bg-[#F3EFE6] dark:bg-blue-900 mt-4">
+            <View className={cn('py-4 flex-row items-start', containerClassName)}>
+                <Pressable
+                    className="flex-row items-center gap-4 lg:pr-4 xl:pr-8 hidden lg:flex"
+                    onPress={() => {
+                        if (router.canDismiss()) {
+                            router.dismissAll();
+                        }
+                        router.replace('/');
+                    }}
+                >
+                    <Image source={appIconData} className="w-12 h-12 rounded shadow-blue-50 shadow-xs dark:shadow-none" />
+
+                    <Text variant="header-lg" color="subtle">
+                        {appConfig.app.name}
+                    </Text>
+                </Pressable>
+
                 <View className="items-center gap-1 flex-1">
                     <Text variant="header" color="subtle">
                         {getTranslation('account.title')}
@@ -23,6 +43,36 @@ export const Footer: React.FC = () => {
                     </Link>
                     <Link color="subtle" href="/more/settings">
                         {getTranslation('settings.title')}
+                    </Link>
+                </View>
+
+                <View className="items-center gap-1 flex-1">
+                    <Text variant="header" color="subtle">
+                        Ongoing Matches
+                    </Text>
+
+                    {authProfileId && (
+                        <Link color="subtle" href="/matches/live/mine">
+                            {getTranslation('footer.mine')}
+                        </Link>
+                    )}
+
+                    {profileIds?.length ? (
+                        <Link color="subtle" href="/matches/live/following">
+                            {getTranslation('footer.following')}
+                        </Link>
+                    ) : null}
+
+                    <Link color="subtle" href="/competitive/games">
+                        {getTranslation('competitive.title')}
+                    </Link>
+
+                    <Link color="subtle" href="/matches/live/lobbies">
+                        {getTranslation('lobbies.title')}
+                    </Link>
+
+                    <Link color="subtle" href="/matches/live/all">
+                        {getTranslation('footer.all')}
                     </Link>
                 </View>
 
@@ -41,20 +91,22 @@ export const Footer: React.FC = () => {
                         {getTranslation('privacy.title')}
                     </Link>
                 </View>
-
-                <View className="items-center gap-1 flex-1">
-                    <Text variant="header" color="subtle">
-                        {getTranslation('footer.community')}
-                    </Text>
-
-                    <Link color="subtle" href="https://discord.com/invite/gCunWKx">
-                        {getTranslation('account.discord.title')}
-                    </Link>
-                    <Link color="subtle" href="https://www.buymeacoffee.com/denniskeil">
-                        {getTranslation('footer.buymeacoffee')}
-                    </Link>
-                </View>
             </View>
+
+            {Platform.OS === 'web' && (
+                <View className={cn(containerClassName, 'py-4 web:max-w-3xl! justify-center flex-row gap-4')}>
+                    &nbsp;&nbsp;
+                    <a href="https://discord.gg/gCunWKx" target="_blank">
+                        <img src="https://img.shields.io/discord/727175083977736262.svg?label=Discord&logo=discord&logoColor=ffffff&labelColor=7289DA&color=2c2f33" />
+                    </a>
+                    <a href="https://www.buymeacoffee.com/denniskeil" target="_blank">
+                        <img src="https://img.shields.io/endpoint.svg?url=https%3A%2F%2Fshields-io-buymeacoffee.vercel.app%2Fapi%3Fusername%3Ddenniskeil" />
+                    </a>
+                    <a href="https://github.com/denniske/aoe2companion" target="_blank">
+                        <img src={`https://img.shields.io/badge/github-aoe2companion-brightgreen?label=Github&logo=github`} />
+                    </a>
+                </View>
+            )}
 
             <View className={cn(containerClassName, 'py-4 web:max-w-3xl! justify-center')}>
                 <Text variant="body-sm" color="subtle" align="center" className="inline-block">
