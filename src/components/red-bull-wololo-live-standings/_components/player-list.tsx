239b65af1c1ac5da8c38bac23f77566869bec6e4
Noah Brandyberry <noahbrandyberry@gmail.com>
2026-01-02 14:21:13 -0500

    chore: don't show error

--- src/components/red-bull-wololo-live-standings/_components/player-list.tsx
+++ src/components/red-bull-wololo-live-standings/_components/player-list.tsx
@@ -112,7 +112,7 @@ export function PlayerList({
                     throw Error('Leaderboard bug');
                 }
 
-                if (!leaderboardData?.matches?.length) {
+                if (!leaderboardData?.matches) {
                     throw Error('Matches bug');
                 }
 
