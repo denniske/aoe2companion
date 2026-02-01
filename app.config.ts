d7ef9b96d731cc4e31f6a2e3451e0e11dc64b7fe
Dennis Keil <dennis.keil10@gmail.com>
2026-02-01 18:41:53 +0000

    v182 & v32

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import { ConfigContext, ExpoConfig } from 'expo/config';
 
-const versionAoe2 = '181.0.0';
-const versionAoe4 = '31.0.0';
+const versionAoe2 = '182.0.0';
+const versionAoe4 = '32.0.0';
 
 console.log('Building for', process.env.GAME, process.env.EAS_BUILD_PROFILE, process.env.EAS_BUILD_RUNNER);
 
