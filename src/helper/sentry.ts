4fc5284bdaee4104919d4f5d30b041c7e3132d45
Dennis Keil <dennis.keil10@gmail.com>
2026-06-04 12:12:18 +0900

    Do not report on handled file downloads from cacheLiveActivityAssets()

--- src/helper/sentry.ts
+++ src/helper/sentry.ts
@@ -8,6 +8,18 @@ export default function initSentry() {
         debug: false,
         enabled: !__DEV__ && Platform.OS !== 'web',
 
+        beforeSend: (event, hint) => {
+            const isHandled = event.exception?.values?.some((ex) => ex.mechanism?.handled === true);
+            const message = event.exception?.values?.[0]?.value;
+
+            // Do not report on handled file downloads from cacheLiveActivityAssets()
+            if (isHandled && message === 'Unable to download a file: The request timed out.') {
+                return null;
+            }
+
+            return event;
+        },
+
         // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
         // We recommend adjusting this value in production.
         // tracesSampleRate: 1.0,
