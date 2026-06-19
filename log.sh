b9a6af565bb3424f63054f87acd257b00b0cae2f
Dennis Keil <dennis.keil10@gmail.com>
2026-06-19 21:52:03 +0700

    show log errors also

--- log.sh
+++ log.sh
@@ -3,4 +3,4 @@ export SERVICE_NAME=web
 export APP_NAME=${GAME}-${SERVICE_NAME}
 export IP=23.88.13.76
 
-ssh -o StrictHostKeyChecking=no root@${IP} "docker ps -q -f name=${APP_NAME}.web.* | xargs -L 1 -P `docker ps | wc -l` docker logs --since 1h -f"
+ssh -o StrictHostKeyChecking=no root@${IP} "docker ps -q -f name=${APP_NAME}.web.* | xargs -L 1 -P `docker ps | wc -l` docker logs --since 1h -f 2>&1"
