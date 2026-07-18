1e621d940d5aa29f6b8ce85bf0a97b84ab02e5f2
Dennis Keil <dennis.keil10@gmail.com>
2026-07-18 10:16:35 +0200

    cleanup

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -109,7 +109,7 @@ export const AccountMenu: React.FC = () => {
                                     className="flex flex-row w-full items-center gap-2 rounded-lg px-3 py-1.5 hover:bg-hoverBackground"
                                 >
                                     <Icon icon={faArrowRightToBracket} />
-                                    Login
+                                    <Text variant="body-lg">Login</Text>
                                 </Pressable>
                             </MenuItem>
                         )}
